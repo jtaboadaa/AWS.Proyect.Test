6d3e0b522975656db8642dd5ee77d8e1fb96bf7f
dgpena <dgpena@uninorte.edu.co>
2021-04-14 21:32:07 -0500

    Response PHP

---
+++ Android Json/includes/DbOperations.php
@@ -0,0 +1,56 @@
+<?php 
+
+	class DbOperations{
+
+		private $con; 
+
+		function __construct(){
+
+			require_once dirname(__FILE__).'/DbConnect.php';
+
+			$db = new DbConnect();
+
+			$this->con = $db->connect();
+
+		}
+
+		/*CRUD -> C -> CREATE */
+
+		public function createCoorde($fecha,$hora, $latitud, $longitud){
+
+				$stmt = $this->con->prepare("INSERT INTO `ubicacion` (`fecha`, `hora`, `latitud`, `longitud`) VALUES (?, ?, ?, ?);");
+				$stmt->bind_param($fecha,$hora,$latitud,$longitud);
+
+				if($stmt->execute()){
+					return 1; 
+				}else{
+					return 2; 
+				}
+			
+		
+
+		public function userLogin($username, $pass){
+			$stmt = $this->con->prepare("SELECT id FROM users WHERE username = ? AND password = ?");
+			$stmt->bind_param("ss",$username,$password);
+			$stmt->execute();
+			$stmt->store_result(); 
+			return $stmt->num_rows > 0; 
+		}
+
+		public function getUserByUsername($username){
+			$stmt = $this->con->prepare("SELECT * FROM users WHERE username = ?");
+			$stmt->bind_param("s",$username);
+			$stmt->execute();
+			return $stmt->get_result()->fetch_assoc();
+		}
+		
+
+		private function isUserExist($username, $email){
+			$stmt = $this->con->prepare("SELECT id FROM users WHERE username = ? OR email = ?");
+			$stmt->bind_param("ss", $username, $email);
+			$stmt->execute(); 
+			$stmt->store_result(); 
+			return $stmt->num_rows > 0; 
+		}
+
+	}
