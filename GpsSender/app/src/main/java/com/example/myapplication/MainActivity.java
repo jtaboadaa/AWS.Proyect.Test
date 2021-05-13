80132c008f1a6311e1a92c9e4c6d8db82de527cf
dgpena <dgpena@uninorte.edu.co>
2021-05-13 09:22:02 -0500

    Correccion UDP

--- GpsSender/app/src/main/java/com/example/myapplication/MainActivity.java
+++ GpsSender/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,65 +5,124 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
-import android.telephony.SmsManager;
+import android.os.StrictMode;
+import android.provider.Settings;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ByteArrayPool;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import kotlin.Metadata;
+import kotlin.jvm.internal.Intrinsics;
+import org.jetbrains.annotations.NotNull;
+
+
+
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
-    TextView tv;
+    TextView tvFecha;
+    TextView tvHora;
+    TextView tvLongitud;
+    TextView tvLatitud;
+    Button btnEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = (TextView)findViewById(R.id.textView);
-        btn = findViewById(R.id.button);
+        tvFecha = (TextView)findViewById(R.id.tvFecha);
+        tvHora = (TextView)findViewById(R.id.tvHora);
+        tvLongitud = (TextView)findViewById(R.id.tvLongitud);
+        tvLatitud = (TextView)findViewById(R.id.tvLatitud);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        btnEnviar = (Button) findViewById(R.id.btnEnviar);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+
+            @SuppressWarnings("deprecation")
             @Override
             public void onClick(View v) {
-                // Acquire a reference to the system Location Manager
-                LocationManager locationManager = (LocationManager) MainActivity.this.getSystemService(Context.LOCATION_SERVICE);
 
-                // Define a listener that responds to location updates
+                String date = String.valueOf(android.text.format.DateFormat.format("MM-dd-yyyy", new java.util.Date()));
+                String hour = String.valueOf(android.text.format.DateFormat.format("HH:mm", new java.util.Date()));
+
+                tvFecha.setText(date);
+                tvHora.setText((hour));
+
+                LocationManager locationManager = (LocationManager) MainActivity.this.getSystemService(Context.LOCATION_SERVICE);
                 LocationListener locationListener = new LocationListener() {
                     public void onLocationChanged(Location location) {
+
                         // Called when a new location is found by the network location provider.
-                        tv.setText(" Latitud: "+location.getLatitude()+" //Longitud: "+location.getLongitude());
+                        tvLatitud.setText(""+ location.getLatitude());
+                        tvLongitud.setText(""+location.getLongitude());
                     }
 
-                    public void onStatusChanged(String provider, int status, Bundle extras) {}
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+                    }
 
-                    public void onProviderEnabled(String provider) {}
+                    public void onProviderEnabled(String provider) {
+                    }
 
-                    public void onProviderDisabled(String provider) {}
+                    public void onProviderDisabled(String provider) {
+                    }
+
+                    ;
+
                 };
-
                 // Register the listener with the Location Manager to receive location updates
                 int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+
                 locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-
                 MessageSender messageSender = new MessageSender();
-                messageSender.execute(tv.getText().toString());
+                messageSender.execute(tvFecha.getText().toString(),tvHora.getText().toString(),tvLatitud.getText().toString(),tvLongitud.getText().toString());
+                Toast.makeText(MainActivity.this, "Ubicacion Enviada", Toast.LENGTH_LONG).show();
 
-                Toast.makeText(MainActivity.this, "Ubicacion  Enviada", Toast.LENGTH_LONG).show();
             }
         });
-
         int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
 
         if(permissionCheck == PackageManager.PERMISSION_DENIED){
+
             if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
 
             } else {
@@ -72,10 +131,9 @@ public class MainActivity extends AppCompatActivity {
                         1);
             }
         }
-
-
     }
+
+
+
+
 }
-
-
-
--- GpsSender/app/src/main/java/com/example/myapplication/MessageSender.java
+++ GpsSender/app/src/main/java/com/example/myapplication/MessageSender.java
@@ -1,35 +1,71 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.Socket;
+
+import kotlin.Metadata;
+import kotlin.jvm.internal.Intrinsics;
 
 public class MessageSender extends AsyncTask<String,Void,Void>
 {
-    Socket s;
-    DataOutputStream dos;
+    DataOutputStream cuatro;
+
+    private String[] RemoteHosts = new String[]{"34.193.232.152", "54.144.9.5", "34.22.169.125", "52.203.18.125"};
+    private int RemotePort = 37777;
     PrintWriter pw;
 
+    @NotNull
+    public final String[] getHosts() {
+        return this.RemoteHosts;
+    }
+
+    public final int getPort() {
+        return this.RemotePort;
+    }
+
+    public void setCustomHost(@NotNull String host) {
+        Intrinsics.checkNotNullParameter(host, "host");
+        this.RemoteHosts[4] = host;
+
+    }
+    @SuppressWarnings("deprecation")
     @Override
-    protected Void doInBackground (String... voids) {
+    protected Void doInBackground(String... voids) {
+
         String message = voids [0];
-        try
-        {
-            s = new Socket ("34.193.232.152",1337);
-            pw = new PrintWriter(s.getOutputStream());
-            pw.write(message);
-            pw.flush();
-            pw.close();
-            s.close();
-        }catch (IOException e)
-        {
+        Intrinsics.checkNotNullParameter(message, "message");
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            DatagramSocket datagramSocket = new DatagramSocket();
+            for (int i = 0; i < getHosts().length; i++) {
+                DatagramPacket datagramPacket = new DatagramPacket(this.RemoteHosts[i].getBytes(),this.RemoteHosts[i].length() , InetAddress.getByName(this.RemoteHosts[i]), this.getPort());
+                datagramSocket.send(datagramPacket);
+                pw = new PrintWriter(String.valueOf(datagramSocket));
+
+            }        } catch (Exception e) {
             e.printStackTrace();
         }
-
         return null;
     }
+
 }
+
+
