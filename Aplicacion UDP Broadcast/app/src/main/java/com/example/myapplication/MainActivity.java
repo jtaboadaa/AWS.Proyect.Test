aecacb0473a950cd3e9f9310d8c008ff9f9df5e5
dgpena <dgpena@uninorte.edu.co>
2021-05-19 09:50:43 -0500

    UDP broadcast

---
+++ Aplicacion UDP Broadcast/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,158 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.StrictMode;
+import android.provider.Settings;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ByteArrayPool;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import kotlin.Metadata;
+import kotlin.jvm.internal.Intrinsics;
+import org.jetbrains.annotations.NotNull;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ExecutorService executorService = Executors.newFixedThreadPool(1);
+    private final int MILISEGUNDOS_ESPERA = 3000;
+
+    TextView tvFecha;
+    TextView tvHora;
+    TextView tvLongitud;
+    TextView tvLatitud;
+    Button btnEnviar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvFecha = (TextView)findViewById(R.id.tvFecha);
+        tvHora = (TextView)findViewById(R.id.tvHora);
+        tvLongitud = (TextView)findViewById(R.id.tvLongitud);
+        tvLatitud = (TextView)findViewById(R.id.tvLatitud);
+
+        btnEnviar = (Button) findViewById(R.id.btnEnviar);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onClick(View v) {
+                String date = String.valueOf(android.text.format.DateFormat.format("MM-dd-yyyy", new java.util.Date()));
+                String hour = String.valueOf(android.text.format.DateFormat.format("HH:mm", new java.util.Date()));
+
+                tvFecha.setText(date);
+                tvHora.setText((hour));
+
+                LocationManager locationManager = (LocationManager) MainActivity.this.getSystemService(Context.LOCATION_SERVICE);
+                LocationListener locationListener = new LocationListener() {
+                    public void onLocationChanged(Location location) {
+
+                        // Called when a new location is found by the network location provider.
+                        tvLatitud.setText(""+ location.getLatitude());
+                        tvLongitud.setText(""+location.getLongitude());
+                    }
+
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+                    }
+
+                    public void onProviderEnabled(String provider) {
+                    }
+
+                    public void onProviderDisabled(String provider) {
+                    }
+
+                    ;
+
+                };
+                Toast.makeText(MainActivity.this, "Ubicacion Enviada", Toast.LENGTH_LONG).show();
+
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            int permissionCheck1 = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+                            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 3000, 0, locationListener);
+
+                            String msn = String.format(tvFecha.getText().toString(), tvHora.getText().toString(), tvLatitud.getText().toString(), tvLongitud.getText().toString());
+                            MessageSender sender = new MessageSender(msn);
+                            executorService.submit(sender);
+                            handler.postDelayed(this, MILISEGUNDOS_ESPERA);
+                        }
+                    }, MILISEGUNDOS_ESPERA);
+
+
+
+            }
+        });
+
+
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if(permissionCheck == PackageManager.PERMISSION_DENIED){
+
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            } else {
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        1);
+            }
+        }
+    }
+
+
+
+
+}
---
+++ Aplicacion UDP Broadcast/app/src/main/java/com/example/myapplication/MessageSender.java
@@ -0,0 +1,55 @@
+package com.example.myapplication;
+
+
+import android.os.StrictMode;
+import org.jetbrains.annotations.NotNull;
+import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import kotlin.jvm.internal.Intrinsics;
+
+public class MessageSender implements Runnable {
+
+    private String[] RemoteHosts = new String[]{"localhost","34.193.232.152","34.232.169.125","52.203.18.125"};
+    private int RemotePort = 37777;
+    PrintWriter pw;
+
+    public final String[] getHosts() {
+        return this.RemoteHosts;
+    }
+
+    private final String send;
+    public final int getPort() {
+        return this.RemotePort;
+    }
+
+    public void setCustomHost(@NotNull String host) {
+        Intrinsics.checkNotNullParameter(host, "host");
+        this.RemoteHosts[4] = host;
+    }
+
+    public MessageSender(String msn){
+        send = msn;
+    }
+
+    @Override
+    public void run() {
+        try {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().build();
+        StrictMode.setThreadPolicy(policy);
+
+            int i;
+            for (i = 0; i <= 4; i++) {
+                DatagramSocket datagramSocket = new DatagramSocket();
+                DatagramPacket datagramPacket = new DatagramPacket(send.getBytes(),send.length() , InetAddress.getByName(this.RemoteHosts[i]), this.getPort());
+                datagramSocket.send(datagramPacket);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
+
